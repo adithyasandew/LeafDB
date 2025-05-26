3e1b2cd32b1aaf64694377586f8c84e1271950a9
BinMushan <mmbmushan@gmail.com>
2025-05-26 23:06:30 +0530

    use function emplemented

--- storage_engine.c
+++ storage_engine.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+char current_db[256] = ""; // Default is null (empty)
+
 // Function to create a new database (folder in ./data)
 void newdb(const char *db_name)
 {
@@ -38,7 +40,27 @@ void dbs()
     system("ls -1 ./data 2>/dev/null | awk '{print \"- \"$0}'");
 }
 
+void use(const char *db_name)
+{
+    char path[256];
+    snprintf(path, sizeof(path), "./data/%s", db_name);
+    
+    // Check if the database folder exists
+    char check_cmd[300];
+    snprintf(check_cmd, sizeof(check_cmd), "[ -d \"%s\" ]", path);
+    
+    if (system(check_cmd) == 0)
+    {
+        strncpy(current_db, db_name, sizeof(current_db));
+        printf("Using database '%s'\n", current_db);
+    }
+    else
+    {
+        printf("Database '%s' does not exist.\n", db_name);
+    }
+}
 
 int main() {
     return 0;
 }
+
